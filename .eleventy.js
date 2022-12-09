70e078c5a0b1d396914a0aa2c4d4274f327d15a1
Joe Pavitt <joepavitt@flowforge.com>
2022-12-09 15:30:06 +0000

    Improve clipboard & tooltip styling & code formatting

--- .eleventy.js
+++ .eleventy.js
@@ -128,13 +128,12 @@ module.exports = function(eleventyConfig) {
 
     const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
     eleventyConfig.addPlugin(syntaxHighlight)
-	  eleventyConfig.addPlugin(codeClipboard)
-
+	eleventyConfig.addPlugin(codeClipboard)
 
     const markdownLib = markdownIt(markdownItOptions)
-      .use(markdownItAnchor, markdownItAnchorOptions)
-      .use(markdownItFootnote)
-			.use(codeClipboard.markdownItCopyButton)
+        .use(markdownItAnchor, markdownItAnchorOptions)
+        .use(markdownItFootnote)
+		.use(codeClipboard.markdownItCopyButton)
 
     eleventyConfig.setLibrary("md", markdownLib)
 
