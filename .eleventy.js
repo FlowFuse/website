b0f1bc05afe4fc7b39486570a46db2421a89c47b
Joe Pavitt <joepavitt@flowforge.com>
2023-02-03 15:28:04 +0000

    Update handbook template to remove hard-coded handbook references & update docs to use this template

--- .eleventy.js
+++ .eleventy.js
@@ -102,107 +102,114 @@ module.exports = function(eleventyConfig) {
     // Create a collection for sidebar navigation
     eleventyConfig.addCollection('nav', function(collection) {
         let nav = {}
-        
-        collection.getAll().filter((page) => {
-            return page.data.tags?.includes('handbook')
-            // url.indexOf('/handbook') === 0
-        }).sort((a, b) => {
-            // sort by depth, so we catch all the correct index.md routes
-            const hierarchyA = a.url.split('/').filter(n => n)
-            const hierarchyB = b.url.split('/').filter(n => n)
-            return hierarchyA.length - hierarchyB.length
-        }).forEach((page) => {
-            // work out ToC Hierarchy
-            // split the folder URI/URL, as this defines our TOC Hierarchy
-            const hierarchy = page.url.split('/').filter(n => n)
-            // recursively parse the folder hierarchy and created our collection object
-            // pass nav = {} as the first accumulator - build up hierarchy map of TOC
-            hierarchy.reduce((accumulator, currentValue, i) => {
-                // create a nested object detailing the full handbook hierarchy
-                if (!accumulator[currentValue]) {
-                    accumulator[currentValue] = {
-                        'name': currentValue,
-                        'url': page.url,
-                        'children': {}
-                    }
-                    if (page.data.navTitle) {
-                        accumulator[currentValue].name = page.data.navTitle
-                    }
-                    // TODO: navGroup will be used in the rendering of the ToC at a later stage
-                    if (page.data.navGroup) {
-                        accumulator[currentValue].group = page.data.navGroup
-                    }
-                }
-                return accumulator[currentValue].children
-            }, nav)
-        })
 
-        // recursive functions to format our nav map to arrays
-        function childrenToArray (children) {
-            return Object.values(children)
-        }
-        function nestedChildrenToArray (value) {
-            for (const [key, entry] of Object.entries(value)) {
-                if (entry.children && Object.keys(entry.children).length > 0) {
-                    // ensure our grandchildren are all converted to arrays before
-                    // we convert the higher level object to an array
-                    nestedChildrenToArray(entry.children)
-                    // now we have converted all grandchildren,
-                    // we can convert our children to an array
-                    entry.children = childrenToArray(entry.children)
-                } else {
-                    delete entry.children
-                }
-            }
-            
-        }
-        // convert our objects to arrays so we can render in nunjucks
-        nestedChildrenToArray(nav)
+        createNav('handbook')
+        createNav('docs')
 
-        // add functionality to group to-level items for better navigation.
-        let groups = {
-            'Other': {
-                name: 'Other',
-                order: -1,    // always render last
-                children: []
-            }
-        }
-        // not req'd to have handbook in Website build, so this may be empty
-        if (nav.handbook) {
-            for (child of nav.handbook.children) {
-                if (child.group) {
-                    const group = child.group
-                    if (!groups[group]) {
-                        groups[group] = {
-                            name: group,
-                            order: 0,
-                            children: []
+        function createNav (tag) {
+            collection.getAll().filter((page) => {
+                return page.data.tags?.includes(tag) && !page.url.includes('README')
+                // url.indexOf('/handbook') === 0
+            }).sort((a, b) => {
+                // sort by depth, so we catch all the correct index.md routes
+                const hierarchyA = a.url.split('/').filter(n => n)
+                const hierarchyB = b.url.split('/').filter(n => n)
+                return hierarchyA.length - hierarchyB.length
+            }).forEach((page) => {
+                console.log(page.url)
+                // work out ToC Hierarchy
+                // split the folder URI/URL, as this defines our TOC Hierarchy
+                const hierarchy = page.url.split('/').filter(n => n)
+                // recursively parse the folder hierarchy and created our collection object
+                // pass nav = {} as the first accumulator - build up hierarchy map of TOC
+                hierarchy.reduce((accumulator, currentValue, i) => {
+                    // create a nested object detailing the full handbook hierarchy
+                    if (!accumulator[currentValue]) {
+                        accumulator[currentValue] = {
+                            'name': currentValue,
+                            'url': page.url,
+                            'children': {}
+                        }
+                        if (page.data.navTitle) {
+                            accumulator[currentValue].name = page.data.navTitle
+                        }
+                        // TODO: navGroup will be used in the rendering of the ToC at a later stage
+                        if (page.data.navGroup) {
+                            accumulator[currentValue].group = page.data.navGroup
                         }
                     }
-                    groups[group].children.push(child)
-                } else {
-                    // capture & flag top-level handbook docs, that haven't had a group assigned
-                    groups['Other'].children.push(child)
-                }
-            }
-
-            function sortChildren (a, b) {
-                // sort children by 'order', then alphabetical
-                return b.order - a.order || a.name.localeCompare(b.name)
-            }
-
-            nav.handbook.groups = Object.values(groups).sort(sortChildren)
-            
-            nav.handbook.groups.forEach((group) => {
-                if (group.children) {
-                    group.children.forEach((child) => {
-                        if (child.children) {
-                            child.children.sort(sortChildren)
-                        }
-                    })
-                    group.children.sort(sortChildren)
-                }
+                    return accumulator[currentValue].children
+                }, nav)
             })
+    
+            // recursive functions to format our nav map to arrays
+            function childrenToArray (children) {
+                return Object.values(children)
+            }
+            function nestedChildrenToArray (value) {
+                for (const [key, entry] of Object.entries(value)) {
+                    if (entry.children && Object.keys(entry.children).length > 0) {
+                        // ensure our grandchildren are all converted to arrays before
+                        // we convert the higher level object to an array
+                        nestedChildrenToArray(entry.children)
+                        // now we have converted all grandchildren,
+                        // we can convert our children to an array
+                        entry.children = childrenToArray(entry.children)
+                    } else {
+                        delete entry.children
+                    }
+                }
+                
+            }
+            // convert our objects to arrays so we can render in nunjucks
+            nestedChildrenToArray(nav)
+    
+            // add functionality to group to-level items for better navigation.
+            let groups = {
+                'Other': {
+                    name: 'Other',
+                    order: -1,    // always render last
+                    children: []
+                }
+            }
+
+            // not req'd to have handbook in Website build, so this may be empty
+            if (nav[tag]) {
+                for (child of nav[tag].children) {
+                    if (child.group) {
+                        const group = child.group
+                        if (!groups[group]) {
+                            groups[group] = {
+                                name: group,
+                                order: 0,
+                                children: []
+                            }
+                        }
+                        groups[group].children.push(child)
+                    } else {
+                        // capture & flag top-level handbook docs, that haven't had a group assigned
+                        groups['Other'].children.push(child)
+                    }
+                }
+    
+                function sortChildren (a, b) {
+                    // sort children by 'order', then alphabetical
+                    return b.order - a.order || a.name.localeCompare(b.name)
+                }
+    
+                nav[tag].groups = Object.values(groups).sort(sortChildren)
+                
+                nav[tag].groups.forEach((group) => {
+                    if (group.children) {
+                        group.children.forEach((child) => {
+                            if (child.children) {
+                                child.children.sort(sortChildren)
+                            }
+                        })
+                        group.children.sort(sortChildren)
+                    }
+                })
+            }
         }
 
         return nav;
