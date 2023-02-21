b9804f2b14f44a5a6a7eaa566a93e830a6848413
ZJ van de Weg <git@zjvandeweg.nl>
2023-02-21 12:21:49 +0100

    test: silence xml warnings

--- test/title.spec.js
+++ test/title.spec.js
@@ -1,14 +1,22 @@
 const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
+
 describe("Page titles", async function() {
 	it("displays the homepage title", async function() {
 		const data = fs.readFileSync(path.resolve(__dirname, "../_site/index.html"), 'utf8');
 
 		const xpath = require('xpath');
-		const dom = require('xmldom').DOMParser;
+    const dom = require('xmldom').DOMParser;
 
-		var nodes = xpath.select("//title", new dom().parseFromString(data))
+		const doc = new dom({
+        locator: {},
+        errorHandler: { warning: function (w) { }, 
+        error: function (e) { }, 
+        fatalError: function (e) { console.error(e) } }
+    }).parseFromString(data);
+
+		var nodes = xpath.select("//title", doc)
 
 		assert.equal(nodes[0].firstChild.data, "FlowForge • DevOps for Node-RED")
 	})
