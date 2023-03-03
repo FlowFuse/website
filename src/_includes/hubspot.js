be2a1a8daf9d7127de2b64773b1b3b25bd7147ca
Pez Cuckow <email@pezcuckow.com>
2023-03-03 17:16:44 +0100

    Only load HubSpot after first interaction with the page

---
+++ src/_includes/hubspot.js
@@ -0,0 +1,30 @@
+// Set HubSpot Chat to wait a bit so it does not block page load if user has never started a conversation
+if (
+    window.sessionStorage?.getItem("chatInProgress") === null &&
+    window.location.hash !== "#hs-chat-open"
+) {
+    window.hsConversationsSettings = {
+        loadImmediately: false,
+    }
+
+    window.addEventListener("load", (event) => {
+        let loaded = false
+        function loadHubSpotChat(event) {
+            console.log(event)
+            if (loaded) return
+            loaded = true
+            window.HubSpotConversations?.widget?.load()
+        }
+
+        window.addEventListener("mousemove", loadHubSpotChat, { once: true })
+        window.addEventListener("scroll", loadHubSpotChat, { once: true })
+    })
+}
+
+window.hsConversationsOnReady = [
+    () => {
+        window.HubSpotConversations.on("conversationStarted", () => {
+            window.sessionStorage?.setItem("chatInProgress", "true")
+        })
+    },
+]
