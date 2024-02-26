9bf17d0e7b4017b831738dd3a46b5c9ba4f400f1
“yndira-flowforge” <yndira@flowforge.com>
2024-02-26 10:45:46 +0100

    Comment out the blocking script for the HS cookie banner

--- src/_includes/base.js
+++ src/_includes/base.js
@@ -4,63 +4,63 @@
  */
 
 // Third party injected scripts that are not wanted
-const blockedScripts = [
-    /hs-banner.com/ // HubSpot cookie banner that's loaded even if not used
-]
+// const blockedScripts = [
+//     /hs-banner.com/ //  cookie banner that's loaded even if not used
+// ]
 
-function needsToBeBlocked(src) {
-    return blockedScripts.some((blockedScript) => blockedScript.test(src)) 
-}
+// function needsToBeBlocked(src) {
+//     return blockedScripts.some((blockedScript) => blockedScript.test(src)) 
+// }
 
 // Patch document.createElement to allow blocking of unwanted scripts injected externally
-const createElementBackup = document.createElement
-document.createElement = function(...args) {
-    // If this is not a script tag, bypass
-    if(args[0].toLowerCase() !== 'script') {
-        // Binding to document is essential
-        return createElementBackup.bind(document)(...args)
-    }
+// const createElementBackup = document.createElement
+// document.createElement = function(...args) {
+//     // If this is not a script tag, bypass
+//     if(args[0].toLowerCase() !== 'script') {
+//         // Binding to document is essential
+//         return createElementBackup.bind(document)(...args)
+//     }
 
-    const scriptElt = createElementBackup.bind(document)(...args)
+//     const scriptElt = createElementBackup.bind(document)(...args)
 
-    const originalSetAttribute = scriptElt.setAttribute.bind(scriptElt)
+//     const originalSetAttribute = scriptElt.setAttribute.bind(scriptElt)
 
-    // Always set the defer flag
-    originalSetAttribute('defer', 'defer')
+//     // Always set the defer flag
+//     originalSetAttribute('defer', 'defer')
 
-    // Define getters / setters to ensure that the script type is properly set
-    Object.defineProperties(scriptElt, {
-        'src': {
-            get() {
-                return scriptElt.getAttribute('src')
-            },
-            set(value) {
-                if(needsToBeBlocked(value, scriptElt.type)) {
-                    originalSetAttribute('type', 'javascript/blocked')
-                }
-                originalSetAttribute('src', value)
-                return true
-            }
-        },
-        'type': {
-            set(value) {
-                const typeValue =
-                    needsToBeBlocked(scriptElt.src, scriptElt.type) ?
-                    'javascript/blocked' :
-                    value
-                originalSetAttribute('type', typeValue)
-                return true
-            }
-        }
-    })
+//     // Define getters / setters to ensure that the script type is properly set
+//     Object.defineProperties(scriptElt, {
+//         'src': {
+//             get() {
+//                 return scriptElt.getAttribute('src')
+//             },
+//             set(value) {
+//                 if(needsToBeBlocked(value, scriptElt.type)) {
+//                     originalSetAttribute('type', 'javascript/blocked')
+//                 }
+//                 originalSetAttribute('src', value)
+//                 return true
+//             }
+//         },
+//         'type': {
+//             set(value) {
+//                 const typeValue =
+//                     needsToBeBlocked(scriptElt.src, scriptElt.type) ?
+//                     'javascript/blocked' :
+//                     value
+//                 originalSetAttribute('type', typeValue)
+//                 return true
+//             }
+//         }
+//     })
 
-    // Monkey patch the setAttribute function so that the setter is called instead.
-    scriptElt.setAttribute = function(name, value) {
-        if(name === 'type' || name === 'src')
-            scriptElt[name] = value
-        else
-            HTMLScriptElement.prototype.setAttribute.call(scriptElt, name, value)
-    }
+//     // Monkey patch the setAttribute function so that the setter is called instead.
+//     scriptElt.setAttribute = function(name, value) {
+//         if(name === 'type' || name === 'src')
+//             scriptElt[name] = value
+//         else
+//             HTMLScriptElement.prototype.setAttribute.call(scriptElt, name, value)
+//     }
 
-    return scriptElt
-}
+//     return scriptElt
+// }
