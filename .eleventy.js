b74ecc4bfa70d79d3d489f1ac45c2a068ac834f2
sumit shinde ( Roni ) <110285294+sumitshinde-84@users.noreply.github.com>
2024-07-09 10:33:01 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -396,7 +396,7 @@ module.exports = function(eleventyConfig) {
         if (content) {
             const chevronDown = loadSVG('chevron-down')
             return `<li class="${classes}"><a class="flex items-center gap-1">${iconSvg}${label}<span class="ff-nav-chevron">${chevronDown}</span></a>${content}</li>`
-        } else if (link) {
+        } else if (link!==null) {
             return `<li class="${classes}"><a class="flex items-center gap-2" href="${link}">${iconSvg}${label}</a></li>`
         } else {
             return `<li class="${classes}"><span class="flex items-center gap-2">${iconSvg}${label}</span></li>`
