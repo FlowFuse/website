d07206356ed920689d98b5179a7508497bc1bba1
sumit shinde ( Roni ) <110285294+sumitshinde-84@users.noreply.github.com>
2024-07-26 22:51:39 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -684,13 +684,7 @@ module.exports = function(eleventyConfig) {
             if (!href.endsWith('/') && !href.includes('#') && !href.endsWith('.md') && !href.includes('https')) {
                 href += '/';
             }
-    
-            // make sure to update the flowfuse signup URL
-            if (!href.endsWith('/') && href.includes('https://app.flowfuse.com/account/create')) {
-                href += '/';
-            }
 
-    
             tokens[idx].attrs[hrefIndex][1] = href;
         }
         return self.renderToken(tokens, idx, options);
