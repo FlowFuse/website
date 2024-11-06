b37ec7adc20100ed6ec0f2b4872595bd32c5306c
sumit shinde ( Roni ) <shindesumit217@gmail.com>
2024-11-06 15:26:30 +0530

    update old urls with new urls

--- .eleventy.js
+++ .eleventy.js
@@ -744,8 +744,6 @@ module.exports = function(eleventyConfig) {
     return {
         dir: {
             input: "src",
-            output:"_site"
         },
-        passthroughFileCopy: true,
     }
 };
