5092021ad76326615ca5de53815becbd85f8cba6
Joe Pavitt <joepavitt@flowforge.com>
2024-11-27 15:52:53 +0000

    Function client-side filtering and search

--- src/_data/integrations.js
+++ src/_data/integrations.js
@@ -2,7 +2,8 @@ const EleventyFetch = require("@11ty/eleventy-fetch");
 const certifiedNodes = require("./certifiedNodes");
 
 module.exports = async () => {
-    const api = "https://flows.nodered.org/things?format=json&type=node&per_page=200";
+    console.log("Loading Integrations...")
+    const api = "https://ff-integrations.flowfuse.cloud/api/nodes";
 
     const response = await EleventyFetch(api, {
         duration: "4h", // ensure we've gathered new data every 4 hours
@@ -14,13 +15,33 @@ module.exports = async () => {
         acc[node.id] = node;
         return acc;
     }, {});
+    console.log(response.catalogue.length + " integrations loaded.")
     // TODO: Overlap certified nodes here
-    const data = response.data.map((node) => {
+    const data = response.catalogue.map((node) => {
         if (ffNodesMap[node._id]) {
             node.ffCertified = true
         }
+        if (!node.categories) {
+            node.categories = []
+        }
+        // map to ensure we have unique collection names
+        node.categories = node.categories.map(category => {
+            return category.includes('catalogue') ? category : 'catalogue_' + category
+        })
+        if (node.categories.indexOf("catalogue") === -1) {
+            node.categories.push("catalogue")
+        }
         return node        
+    }).sort((a, b) => {
+        if (a.ffCertified && !b.ffCertified) {
+            return -1
+        }
+        if (!a.ffCertified && b.ffCertified) {
+            return 1
+        }
+        return a.name.localeCompare(b.name)
     })
+    console.log("Loaded Integrations.")
 
     return data
 }
