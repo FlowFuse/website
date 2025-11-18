13416a8a5c6be183d26fc9290e0d593b85b9919c
cstns <cstn.serban@gmail.com>
2025-11-18 15:55:44 +0200

    styling

--- src/js/ai-expert-modal.js
+++ src/js/ai-expert-modal.js
@@ -54,8 +54,8 @@ document.addEventListener('DOMContentLoaded', function() {
             if (flowTarget && flowsStore[flowTarget]) {
                 copyToClipboard(flowsStore[flowTarget]);
                 const copyButton = e.target.closest('.copy');
-                const copySvg = copyButton.querySelector('#copy-svg');
-                const checkSvg = copyButton.querySelector('#check-svg');
+                const copySvg = copyButton.querySelector('.copy-svg');
+                const checkSvg = copyButton.querySelector('.check-svg');
                 copySvg.classList.add('hidden');
                 checkSvg.classList.remove('hidden');
                 setTimeout(() => {
@@ -69,8 +69,12 @@ document.addEventListener('DOMContentLoaded', function() {
             const flowSection = e.target.closest('[data-flow-id]');
             if (flowSection) {
                 const preElement = flowSection.querySelector('pre');
+                const downChevron = flowSection.querySelector('.down-arrow');
+                const upChevron = flowSection.querySelector('.up-arrow');
                 if (preElement) {
                     preElement.classList.toggle('hidden');
+                    downChevron.classList.toggle('hidden');
+                    upChevron.classList.toggle('hidden');
                 }
             }
         }
@@ -922,42 +926,60 @@ document.addEventListener('DOMContentLoaded', function() {
             html += '<ul class="space-y-2 overflow-auto">';
 
             richAnswer.flows.forEach(flow => {
-                console.log(flow);
                 if (!flow || typeof flow !== 'object' || !flow.metadata || !Array.isArray(flow.metadata.flows) || flow.metadata.flows.length === 0) return;
                 const flowId = `flow-${crypto.randomUUID()}`
                 let flowsJSON = JSON.stringify(flow.metadata.flows, null, 2);
                 flowsStore[flowId] = flowsJSON;
                 html += `
-                <li class="overflow-auto">
-                    <div class="block p-3 bg-white border border-gray-200 rounded-lg hover:border-indigo-300 hover:bg-gray-50 transition-colors overflow-auto">
-                        <div class="flex flex-col overflow-auto" data-flow-id="${flowId}">
-                            <div class="flex items-start justify-between gap-2">
-                                <h6>${flow.title}</h6>
-                                <div class="actions flex items-start gap-4">
-                                    <button class="text-indigo-600 expand flex items-center">
-                                        <span>JSON</span>
-                                        <svg class="inline-block w-4 h-4 ml-1" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                            <path d="M6 9l6 6 6-6"/>
-                                        </svg>
-                                    </button>
-                                    <button class="text-indigo-600 copy">
-                                      <svg  width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" id="copy-svg">
-                                        <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path>
-                                        <rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect>
-                                      </svg>
-                                      <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" class="hidden" id="check-svg">
-                                        <path d="M20 6L9 17l-5-5"></path>
-                                      </svg>
-                                    </button>
+                <li class="overflow-auto" data-flow-id="${flowId}">
+                    <div class="flex flex-col gap-3 p-3 bg-white border border-gray-200 rounded-lg hover:border-indigo-300 hover:bg-gray-50 transition-colors overflow-auto">
+                        <div class="flex gap-3">
+                            <div class="icon content-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                  <rect width="24" height="24" fill="gray" rx="4"/>
+                                  <g clip-path="url(#a)">
+                                    <path fill="#fff" d="M0 12v-1.647c5.09 0 5.81-.9 6.44-1.695.72-.9 1.46-1.6 3.88-1.6v1.648c-1.76 0-2.04.354-2.51.948C6.79 10.937 5.5 12 0 12Z"/>
+                                    <path fill="#fff" d="M8.6 16.941c-2.9 0-3.47-1.513-3.88-2.614C4.25 13.072 3.85 12 0 12v-1.647c4.67 0 5.67 1.618 6.34 3.419.38 1.015.57 1.522 2.26 1.522v1.647Z"/>
+                                    <path fill="#fff" d="M16.78 19H9.9c-.95 0-1.72-.737-1.72-1.647v-2.47c0-.91.77-1.648 1.72-1.648h6.88c.95 0 1.72.738 1.72 1.647v2.47c0 .91-.77 1.648-1.72 1.648Zm0-4.118H9.9v2.47h6.88v-2.47Zm1.5-4.117H11.4c-.95 0-1.72-.738-1.72-1.647v-2.47c0-.91.77-1.648 1.72-1.648h6.88c.95 0 1.72.737 1.72 1.647v2.47c0 .91-.77 1.648-1.72 1.648Zm0-4.118H11.4v2.47h6.88v-2.47Z"/>
+                                  </g>
+                                  <defs>
+                                    <clipPath id="a">
+                                      <path fill="#fff" d="M0 5h20v14H0z"/>
+                                    </clipPath>
+                                  </defs>
+                                </svg>
+                            </div>
+                            <div class="flex flex-1 flex-col overflow-auto">
+                                <div class="flex items-start justify-between gap-2">
+                                    <h6>${flow.title}</h6>
+                                    <div class="actions flex items-start gap-4">
+                                        <button class="text-gray-600 expand flex items-center plain">
+                                            <span>JSON</span>
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 down-arrow" viewBox="0 0 20 20" fill="currentColor">
+                                              <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                            </svg>
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 up-arrow hidden" viewBox="0 0 20 20" fill="currentColor">
+                                              <path fill-rule="evenodd" d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z" clip-rule="evenodd" />
+                                            </svg>
+                                        </button>
+                                        <button class="text-gray-600 copy plain">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"  class="h-6 w-6 copy-svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                              <path stroke-linecap="round" stroke-linejoin="round" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
+                                            </svg>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" class="h-6 w-6 hidden check-svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                              <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                                            </svg>
+                                        </button>
+                                    </div>
                                 </div>
+                                <p class="text-xs" style="margin-bottom: 0;">
+                                    <span>Category: </span>
+                                    <span>${flow.metadata?.category}</span>
+                                </p>
                             </div>
-                            <p class="text-xs" style="margin-bottom: 0;">
-                                <span>Category: </span>
-                                <span>${flow.metadata?.category}</span>
-                            </p>
-                            <div class="overflow-auto" style="max-height: 300px;">
-                                <pre class="overflow-auto hidden mt-5">${flowsJSON}</pre>
-                            </div>
+                        </div>
+                        <div class="overflow-auto rounded-md text-gray-300 ml-8" style="max-height: 300px; background-color: #404040;">
+                            <pre class="overflow-auto hidden mt-3 py-2 px-4">${flowsJSON}</pre>
                         </div>
                     </div>
                </li>`
