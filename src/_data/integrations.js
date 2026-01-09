5ccdf07def20f24173f528d6f61f64eaad16e4af
sumitshinde-84 <shindesumit217@gmail.com>
2026-01-09 18:01:26 +0530

    remove unused prism handler

--- src/_data/integrations.js
+++ src/_data/integrations.js
@@ -172,88 +172,9 @@ module.exports = async () => {
                 }
 
                 if (nodeDetails.readme) {
-                    // List of valid Prism.js languages (common ones)
-                    const validLanguages = new Set([
-                        'markup', 'html', 'xml', 'svg', 'mathml', 'ssml', 'atom', 'rss',
-                        'css', 'clike', 'javascript', 'js', 'abap', 'abnf', 'actionscript',
-                        'ada', 'agda', 'al', 'antlr4', 'g4', 'apacheconf', 'apex', 'apl',
-                        'applescript', 'aql', 'arduino', 'arff', 'asciidoc', 'adoc', 'aspnet',
-                        'asm6502', 'autohotkey', 'autoit', 'bash', 'shell', 'basic', 'batch',
-                        'bbcode', 'birb', 'bison', 'bnf', 'rbnf', 'brainfuck', 'brightscript',
-                        'bro', 'bsl', 'oscript', 'c', 'csharp', 'cs', 'dotnet', 'cpp', 'cfscript',
-                        'cfc', 'chaiscript', 'cil', 'clojure', 'cmake', 'cobol', 'coffeescript',
-                        'coffee', 'concurnas', 'conc', 'csp', 'coq', 'crystal', 'css-extras',
-                        'd', 'dart', 'dataweave', 'dax', 'dhall', 'diff', 'django', 'jinja2',
-                        'dns-zone-file', 'dns-zone', 'docker', 'dockerfile', 'dot', 'gv',
-                        'ebnf', 'editorconfig', 'eiffel', 'ejs', 'elixir', 'elm', 'etlua',
-                        'erb', 'erlang', 'excel-formula', 'xlsx', 'xls', 'fsharp', 'factor',
-                        'false', 'firestore-security-rules', 'flow', 'fortran', 'ftl', 'gml',
-                        'gamemakerlanguage', 'gcode', 'gdscript', 'gedcom', 'gherkin', 'git',
-                        'glsl', 'go', 'graphql', 'groovy', 'haml', 'handlebars', 'hbs', 'haskell',
-                        'hs', 'haxe', 'hcl', 'hlsl', 'http', 'hpkp', 'hsts', 'ichigojam',
-                        'icon', 'icu-message-format', 'idris', 'idr', 'ignore', 'gitignore',
-                        'hgignore', 'npmignore', 'inform7', 'ini', 'io', 'j', 'java', 'javadoc',
-                        'javadoclike', 'javastacktrace', 'jexl', 'jolie', 'jq', 'jsdoc', 'js-extras',
-                        'json', 'webmanifest', 'json5', 'jsonp', 'jsstacktrace', 'js-templates',
-                        'julia', 'keyman', 'kotlin', 'kt', 'kts', 'kumir', 'kum', 'latex', 'tex',
-                        'context', 'latte', 'less', 'lilypond', 'ly', 'liquid', 'lisp', 'emacs',
-                        'elisp', 'emacs-lisp', 'livescript', 'llvm', 'log', 'lolcode', 'lua',
-                        'makefile', 'markdown', 'md', 'markup-templating', 'matlab', 'mel',
-                        'mizar', 'mongodb', 'monkey', 'moonscript', 'moon', 'n1ql', 'n4js', 'n4jsd',
-                        'nand2tetris-hdl', 'naniscript', 'nani', 'nasm', 'neon', 'nevod', 'nginx',
-                        'nim', 'nix', 'nsis', 'objectivec', 'objc', 'ocaml', 'opencl', 'openqasm',
-                        'qasm', 'oz', 'parigp', 'parser', 'pascal', 'objectpascal', 'pascaligo',
-                        'psl', 'pcaxis', 'px', 'peoplecode', 'pcode', 'perl', 'php', 'phpdoc',
-                        'php-extras', 'plsql', 'powerquery', 'pq', 'mscript', 'powershell',
-                        'processing', 'prolog', 'promql', 'properties', 'protobuf', 'pug',
-                        'puppet', 'pure', 'purebasic', 'pbfasm', 'purescript', 'purs', 'python',
-                        'py', 'q', 'qml', 'qore', 'r', 'racket', 'rkt', 'jsx', 'tsx', 'reason',
-                        'regex', 'rego', 'renpy', 'rpy', 'rest', 'rip', 'roboconf', 'robotframework',
-                        'robot', 'ruby', 'rb', 'rust', 'sas', 'sass', 'scss', 'scala', 'scheme',
-                        'shell-session', 'sh-session', 'shellsession', 'smali', 'smalltalk',
-                        'smarty', 'sml', 'smlnj', 'solidity', 'sol', 'solution-file', 'sln',
-                        'soy', 'sparql', 'rq', 'splunk-spl', 'sqf', 'sql', 'squirrel', 'stan',
-                        'iecst', 'stylus', 'swift', 'systemd', 't4-templating', 't4-cs', 't4',
-                        't4-vb', 'tap', 'tcl', 'tt2', 'textile', 'toml', 'tremor', 'trickle',
-                        'troy', 'turtle', 'trig', 'twig', 'typescript', 'ts', 'typoscript',
-                        'tsconfig', 'unrealscript', 'uscript', 'uc', 'uri', 'url', 'v', 'vala',
-                        'vbnet', 'velocity', 'verilog', 'vhdl', 'vim', 'visual-basic', 'vb',
-                        'vba', 'warpscript', 'wasm', 'wiki', 'xeora', 'xeoracube', 'xml-doc',
-                        'xojo', 'xquery', 'yaml', 'yml', 'yang', 'zig'
-                    ]);
-
-                    // Normalize code block languages
-                    // First, replace any code block with invalid or unrecognized language
+                    // Fix relative image paths to use GitHub raw content
                     node.readme = nodeDetails.readme
-                        .replace(/```(\w+)/g, (match, lang) => {
-                            const lowerLang = lang.toLowerCase();
-                            
-                            // Map common aliases and invalid languages to valid ones
-                            const languageMap = {
-                                'jsonl': 'json',
-                                'jsonata': 'json',
-                                'vue': 'javascript',
-                                'console': 'bash',
-                                'cd': 'bash',
-                                'terminal': 'bash',
-                                'cmd': 'bash',
-                                'sh': 'bash'
-                            };
-                            
-                            // Check if it's a mapped language
-                            if (languageMap[lowerLang]) {
-                                return '```' + languageMap[lowerLang];
-                            }
-                            
-                            // Check if it's a valid Prism language
-                            if (validLanguages.has(lowerLang)) {
-                                return '```' + lowerLang;
-                            }
-                            
-                            // Use no language identifier for invalid ones (renders as plain text)
-                            return '```';
-                        })
-                        // Fix relative image paths to use GitHub raw content - markdown style
+                        // Fix relative image paths in markdown style
                         .replace(
                             /!\[(.*?)\]\((?!https?:\/\/)([^)]+)\)/g,
                             (match, alt, imagePath) => {
