11ed26178e96b3b6275632ab5e6258ddae7a2067
Zeger-Jan van de Weg <ZJvandeWeg@users.noreply.github.com>
2026-06-05 03:54:05 -0700

    nuxt: Image optimizing through nuxt/image (#5145)

---
+++ nuxt/components/content/ProseImg.vue
@@ -0,0 +1,11 @@
+<template>
+    <NuxtImg :src="src" :alt="alt" :title="title" />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    src?: string
+    alt?: string
+    title?: string
+}>()
+</script>
