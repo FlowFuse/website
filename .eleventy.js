f1d87f8c4b38bef33b6d0e0369d1506f3beb9535
Yndira Escobar <129537638+Yndira-E@users.noreply.github.com>
2026-07-29 16:00:28 +0200

    Fix Fisher-Yates shuffle and clean up legacy dev-proxy middleware (#5394)

--- .eleventy.js
+++ .eleventy.js
@@ -103,7 +103,12 @@ module.exports = function(eleventyConfig) {
     });
 
       eleventyConfig.addFilter("shuffle", (array) => {
-        return array.sort(() => Math.random() - 0.5);
+        const shuffled = [...array];
+        for (let i = shuffled.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+        }
+        return shuffled;
     });
 
     // Add a global data variable for the current date
