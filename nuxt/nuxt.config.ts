2a36ee08224b2bcd0ad16be07062a519dc89366c
sumitshinde-84 <shindesumit217@gmail.com>
2026-09-01 20:30:33 +0530

    Revert "Fix broken links to /integrations/opcua in blog posts"
    This reverts commit 3136ca34aaecf36e60ff806ca82d1293ab4979c9.

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -374,7 +374,6 @@ export default defineNuxtConfig({
                     '/terms',
                     '/privacy-policy',
                     '/integrations',
-                    '/integrations/opcua',
                     '/pricing',
                     '/product',
                     // /ai is only linked from 11ty-generated HTML (nav, homepage), which the
